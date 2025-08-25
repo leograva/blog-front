36327aa431b0c776b1ff741cc6fea7d468272180
Leonardo Grava de Mendonça <leonardo.grava@outlook.com>
2025-08-24 23:19:40 -0300

    arrumando testes

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  testEnvironment: 'jsdom',
+  moduleDirectories: ['node_modules', 'src'],
+  moduleNameMapper: {
+    '^react-router-dom$': '<rootDir>/node_modules/react-router-dom',
+  },
+  transform: {},
+};
