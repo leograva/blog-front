e5e3e7bc4459f73879836401b8e8a75c191efd89
Leonardo Grava de Mendonça <leonardo.grava@outlook.com>
2025-08-26 14:01:31 -0300

    alterando cicd

--- src/components/__tests__/Header.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import Header from '../Header';
-
-describe('Header', () => {
-  it('renderiza o título do blog', () => {
-    render(<Header />);
-    expect(screen.getByText(/Blog de Postagens/i)).toBeInTheDocument();
-  });
-});
